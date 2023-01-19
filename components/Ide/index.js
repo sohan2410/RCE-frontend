6b15fa31cf4d5d791dfb73a08573a67bb00d7fee
sohan2410 <bandarysohan24@gmail.com>
2023-01-19 14:40:56 +0530

    integrating code/execute API

--- components/Ide/index.js
+++ components/Ide/index.js
@@ -15,25 +15,16 @@ languages.forEach((lang) => {
   require(`ace-builds/src-noconflict/mode-${lang}`)
   require(`ace-builds/src-noconflict/snippets/${lang}`)
 })
-export default function Ide({ lang }) {
+export default function Ide({ lang, text, onChange }) {
   const [mode, setMode] = useState(lang === "c" || lang === "cpp" ? "c_cpp" : lang)
-  const [text, setText] = useState(boilerplate(lang))
   const [fontSize, setFontSize] = useState(14)
-  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
-    setLoading(true)
-    setText(boilerplate(lang))
     if (lang === "c" || lang === "cpp") setMode("c_cpp")
     else {
       setMode(lang)
     }
-    setLoading(false)
   }, [lang])
-
-  const onChange = (value) => {
-    setText(value)
-  }
 
   return (
     <>
@@ -43,18 +34,17 @@ export default function Ide({ lang }) {
         <option value={18}>18</option>
         <option value={20}>20</option>
       </select>
-      {!loading ? (
-        <AceEditor
-          mode={mode}
-          theme="solarized_dark"
-          fontSize={fontSize}
-          onChange={onChange}
-          name="UNIQUE_ID_OF_DIV"
-          defaultValue={text}
-          editorProps={{ $blockScrolling: true }}
-          setOptions={{ enableBasicAutocompletion: true, enableLiveAutocompletion: true }}
-        />
-      ) : null}
+      <AceEditor
+        mode={mode}
+        theme="solarized_dark"
+        fontSize={fontSize}
+        onChange={onChange}
+        name="UNIQUE_ID_OF_DIV"
+        // defaultValue={text}
+        value={text}
+        editorProps={{ $blockScrolling: true }}
+        setOptions={{ enableBasicAutocompletion: true, enableLiveAutocompletion: true }}
+      />
     </>
   )
 }
--- interface/Dashboard/index.js
+++ interface/Dashboard/index.js
@@ -5,12 +5,35 @@ import InputBox from "../../components/InputBox"
 import OutputBox from "../../components/OutputBox"
 import { FaPython, FaCuttlefish } from "react-icons/fa"
 import { DiJavascript1, DiJava } from "react-icons/di"
+import axios from "axios"
 
 const Ide = dynamic(() => import("../../components/Ide"), { ssr: false })
 
 export default function Dashboard() {
   const [lang, setLang] = useState("cpp")
-
+  const [loading, setLoading] = useState(false)
+  const [text, setText] = useState("")
+  const [output, setOutput] = useState("")
+  const handleSubmit = async () => {
+    setLoading(true)
+    setOutput("")
+    const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/code/execute`
+    const format = { javascript: "js", c: "c", cpp: "cpp", java: "java", python: "py" }
+    try {
+      const { data } = await axios.post(url, {
+        format: format[lang],
+        code: text,
+      })
+      if (data.status) setOutput(data.output)
+      console.log(data)
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  const onChange = (value) => {
+    setText(value)
+  }
   const Sidebar = () => {
     return (
       <div className="w-20">
@@ -52,6 +75,9 @@ export default function Dashboard() {
       </div>
     )
   }
+  useEffect(() => {
+    setText(boilerplate(lang))
+  }, [lang])
   return (
     <div className="h-5/6 flex">
       <Sidebar />
@@ -59,15 +85,23 @@ export default function Dashboard() {
       <div className="w-full flex-col h-full">
         <div className="p-1 w-full flex justify-between">
           <p>Language</p>
-          <button className="py-1 px-3 m-2 bg-purple-500 rounded-md hover:bg-purple-800 dark:hover:bg-green-800 dark:bg-green-500">Run</button>
+          <button
+            className="py-1 px-3 m-2 bg-purple-500 rounded-md hover:bg-purple-800 dark:hover:bg-green-800 dark:bg-green-500"
+            onClick={handleSubmit}
+          >
+            Run
+          </button>
         </div>
         <div className=" w-full flex ">
           <div className="w-2/3 h-full">
-            <Ide lang={lang} />
+            <Ide lang={lang} text={text} onChange={onChange} />
           </div>
           <div className="w-1/3 h-full">
-            <div className="h-1/2">input box</div>
-            <div className=" h-1/2">output box</div>
+            <div className="h-1/2">
+              {loading && <span>Loading...</span>}
+              {output && <span>{output}</span>}
+            </div>
+            {/* <div className=" h-1/2">output box</div> */}
           </div>
         </div>
       </div>
