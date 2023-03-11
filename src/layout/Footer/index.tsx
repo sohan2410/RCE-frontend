1a48099661d4444ad0996dbf29ccf563e118d8d4
shivam saraswat <shivam@gmail.com>
2023-03-11 18:27:31 +0530

    footer updated

--- src/layout/Footer/index.tsx
+++ src/layout/Footer/index.tsx
@@ -1,9 +1,5 @@
 import React from "react"
 
 export default function Footer() {
-  return (
-    <div className="w-full text-center p-4">
-      <span className="text-purple-800 dark:text-green-500">©copyright</span>-All right reserved
-    </div>
-  )
+  return <div className="w-full text-center p-4 flex-end">{/* <span className="text-purple-800 dark:text-green-500">©copyright</span>-All right reserved */}</div>
 }
